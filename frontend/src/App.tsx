0afc4c9aa83623ab77aed45763efc12319fbfbf7
Palm Suksawasdi Na Ayuthaya <z5625225@unsw.edu.au>
2026-04-04 16:03:02 +1100

    Set1 register

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,16 @@ import { useState } from 'react'
 import './App.css'
 import LandingPage from './pages/LandingPage';
 import Login from './pages/Login';
+import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<LandingPage/>} />
-      <Route path='/login' element={<Login/>} />
-      <Route path='/register' element={<LandingPage/>} />
+      <Route path='/' element={<LandingPage />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
       <Route path='/dashboard' element={<Dashboard/>} />
     </Routes>
   )
